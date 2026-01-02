78e49ad5c6a9de7d243e21b8212b5db2ba2f731a
DuSiWaNiKhrai <hintspincat@gmail.com>
2026-01-02 13:27:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,21 +8,20 @@ if (key === passwordKey){
     localStorage.setItem("permission" , "true");
     window.location.href = "/qrPage"
 }
-else if(localStorage.getItem("permission") === "true"){
+else if(localStorage.getItem("permission") == "true" ){
     document.getElementById("nextBtn").style.display="flex";
 }
-else{
+else {
     document.getElementById("nextBtn").style.display="none";
 }
-
-function pages(nextPage){
-    document.querySelectorAll(".page").forEach(currentPage =>{currentPage.style.display="none";});
-    document.getElementById(`page${nextPage}`).style.display="flex";
-}
-
 
 // ชั่วคราว
 if(new URLSearchParams(window.location.search).get("reset") === "okay"){
     localStorage.removeItem("permission")
     window.location.href="/"
+}
+
+function pages(nextPage){
+    document.querySelectorAll(".page").forEach(currentPage =>{currentPage.style.display="none";});
+    document.getElementById(`page${nextPage}`).style.display="flex";
 }
