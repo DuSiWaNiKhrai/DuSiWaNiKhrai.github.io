c32f899e0a1f29b5c0a544c084ff081a155e951c
DuSiWaNiKhrai <hintspincat@gmail.com>
2026-01-23 16:32:19 +0700

    Update script.js

--- qrPage/script.js
+++ qrPage/script.js
@@ -1,5 +1,6 @@
 const envelope = document.getElementById("envelope");
 const note = document.getElementById("note");
+let countFlame = 3;
 
 envelope.addEventListener("click",function(){
     this.classList.add("open")
@@ -12,4 +13,24 @@ note.addEventListener("click", function(){
 function nextPage(nextPage){
     document.querySelectorAll(".page").forEach(currentPage =>{currentPage.style.display="none";});
     document.getElementById(`page${nextPage}`).style.display="flex";
+}
+
+function closePopUp(){
+    document.querySelector(".pop-up").style.opacity=0;
+    setTimeout(()=>{document.querySelector(".pop-up").style.display="none";},1000)
+}
+function cancelFlame(numberOfFlame){
+    let flames;
+    if(numberOfFlame === 1 ){
+        flames = document.querySelector(`.flame`);
+    }
+    else{
+        flames = document.querySelector(`.flame${numberOfFlame}`);
+    }
+    countFlame -= 1;
+    flames.style.opacity = 0;
+
+    if(countFlame <= 0){
+        document.querySelector(".page4").classList.add("show");
+    }
 }
